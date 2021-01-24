ee2be759e42cbf20914a525d7991f84e8be3d2eb
root <root@sterner.bs>
2021-01-24 10:50:45 +0000

    Refactor references to files

--- generator.py
+++ generator.py
@@ -3,16 +3,24 @@ import time
 import os
 import shutil
 import hashlib
+import logging
 
-from logger import logger
 
 config = {
-    'blocked_domains_file': 'named.conf.blockeddomains',
     'url': 'https://www.someonewhocares.org/hosts/hosts',
+    'blocked_domains_file': 'named.conf.blockeddomains',
+    'additional_domains_file': 'additional_domains',
+    'current_additional_domains_file': '.cad',
+    'raw_parsed_file': '.raw',
+    'fresh_blacklist_file': '.fbl',
+    'stale_blacklist_file': '.sbl',
     'wait_time': 10,
-    'target_path': '/etc/named/'
+    'blocked_domains_target_path': '/etc/named/'
 }
 
+FORMAT = '%(asctime)s: %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+logger = logging.getLogger()
 
 def apply_bind_format(line):
     prefix = 'zone "'
@@ -73,7 +81,7 @@ def create_domain_list_file():
     logger.debug(f"Downloading domain list from {config['url']}")
     r = requests.get(config['url'], stream=True)
 
-    with open('new_hosts', 'wb') as domain_list:
+    with open(config['fresh_blacklist_file'], 'wb') as domain_list:
         logger.debug('Writing domain list to local file')
         for chunk in r.iter_content(chunk_size=80):
             if chunk:
@@ -87,69 +95,71 @@ def get_hash(item):
 
 
 def has_diff():
-    if not os.path.exists('./old_hosts'):
+    if not os.path.exists(config['stale_blacklist_file']):
         return False
 
-    logger.debug('diffing with source file')
-    with open('new_hosts', 'rb') as afile:
+    logger.info('Checking for diff in fresh and stale blacklist file hashes')
+    with open(config['fresh_blacklist_file'], 'rb') as afile:
         buf = afile.read()
         hash1 = get_hash(buf)
-    logger.debug(f"new_hosts hash: {hash1}")
+    logger.debug(f"{config['fresh_blacklist_file']} hash: {hash1}")
 
-    with open('old_hosts', 'rb') as afile:
+    with open(config['stale_blacklist_file'], 'rb') as afile:
         buf = afile.read()
         hash2 = get_hash(buf)
-    logger.debug(f"old_hosts hash: {hash2}")
+    logger.debug(f"{config['stale_blacklist_file']} hash: {hash2}")
 
     if hash1 != hash2:
+        logger.info("A diff in hashes has been found!")
         return True
+    logger.debug("Hashes are identical")
     return False
 
 
 def read_file_to_list(path):
-    f = open(path, 'r')
-    as_list = f.read().splitlines()
-    f.close()
-    return as_list
+    try:
+        f = open(path, 'r')
+        as_list = f.read().splitlines()
+        f.close()
+        return as_list
+    except FileNotFoundError:
+        logger.warning(f'File {path} not found. Returning empty list...')
+        return []
 
 
 def has_new_additional_domains():
-    additional_domains = read_file_to_list('additional_domains')
-    cad = read_file_to_list('.current_additional_domains')
-    logger.debug(f'desired additional domains: {additional_domains}')
-    logger.debug(f'current additional domains: {cad}')
+    additional_domains = read_file_to_list(config['additional_domains_file'])
+    cad = read_file_to_list(config['current_additional_domains_file'])
+    logger.debug(f'Desired additional domains: {additional_domains}')
+    logger.debug(f'Current additional domains: {cad}')
 
     if len(additional_domains) != len(cad):
-        logger.info('additional domains have been found!')
+        logger.info('Additional domains have been found!')
         write_current_additional_domains()
         return True
-
-    logger.debug('no diff on additional domains')
     return False
 
 
 def write_current_additional_domains():
-    logger.debug('asfasfasf')
-    additional_domains = read_file_to_list('additional_domains')
-    with open('.current_additional_domains', 'w') as cad_output:
+    additional_domains = read_file_to_list(config['additional_domains_file'])
+    with open(config['current_additional_domains_file'], 'w') as cad_output:
         for line in additional_domains:
-            print(line)
             cad_output.write(f'{line}\n')
 
 
 def process_new_hosts():
-    with open('new_hosts', 'r', encoding='utf-8-sig') as raw:
+    with open(config['fresh_blacklist_file'], 'r', encoding='utf-8-sig') as raw:
         data = raw.read().splitlines(True)
-    additional_domains = read_file_to_list('additional_domains')
+    additional_domains = read_file_to_list(config['additional_domains_file'])
 
     blocked_domains = process_blocked_domains_list(data) + process_additional_domains_list(additional_domains)
 
-    with open('raw_parsed_as_bind_file', 'w') as foutput:
+    with open(config['raw_parsed_file'], 'w') as foutput:
         for line in blocked_domains:
             foutput.write(f'{line}\n')
 
     # remove duplicates
-    with open('raw_parsed_as_bind_file') as f:
+    with open(config['raw_parsed_file']) as f:
         seen = set()
         duplicates = []
         for line in f:
@@ -158,49 +168,50 @@ def process_new_hosts():
             else:
                 seen.add(line)
 
-    with open('raw_parsed_as_bind_file', 'r') as file_input:
-        with open(config['blocked_domains_file'], 'w') as output_file:
+    with open(config['raw_parsed_file'], 'r') as file_input:
+        blocked_domains_file = config['blocked_domains_file']
+        with open(blocked_domains_file, 'w') as output_file:
             for line in file_input:
                 if line in duplicates:
                     continue
                 output_file.write(line)
-    logger.debug(f"Written to file: {config['blocked_domains_file']}")
+    logger.debug(f"Written to file: {blocked_domains_file}")
 
 
 def update_blocked_domains():
-    source = f"./{config['blocked_domains_file']}"
-    destination = config['target_path']
+    source = config['blocked_domains_file']
+    destination = config['blocked_domains_target_path']
     try:
         shutil.copy(source, destination)
-        logger.debug(f"updated blocked domains file at {destination}")
+        logger.debug(f"Updated blocked domains file at {destination}")
 
         os.system("rndc reload")
-        logger.debug("reloaded bind")
+        logger.debug("Reloaded bind process using rndc")
     except Exception as ex:
         logger.error(f"An error occured when copying the {source} to {destination}. Exception {ex}")
 
 
-for item in ['./old_hosts', './new_hosts', './raw_parsed_as_bind_file']:
-    logger.debug('cleaning up...')
+for item in ['./old_hosts', './new_hosts', config['raw_parsed_file']]:
+    logger.debug('Cleaning up...')
     try:
         os.remove(item)
     except FileNotFoundError:
-        logger.debug(f'File {item} not found')
+        logger.warning(f'File {item} not found')
 
 
 if __name__ == '__main__':
     create_domain_list_file()
     while True:
         if has_diff() or has_new_additional_domains():
-            logger.debug('it has a diff!')
+            logger.debug('A diff has been found!')
             process_new_hosts()
             update_blocked_domains()
             time.sleep(config['wait_time'])
         else:
-            logger.debug('no diff found, continuing')
+            logger.debug('No diff found, continuing...')
             time.sleep(config['wait_time'])
 
-            logger.debug('renaming new_hosts')
-            os.rename('./new_hosts', './old_hosts')
-            logger.debug('downloading from source')
+            logger.debug('Renaming new_hosts')
+            os.rename(config['fresh_blacklist_file'], config['stale_blacklist_file'])
+            logger.debug('Downloading from source')
             create_domain_list_file()
